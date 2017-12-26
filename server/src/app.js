029e830a35ef504b28675cdc5832feda52c49618
hannahcmtucker <hannahcmtucker@gmail.com>
2017-12-26 14:29:41 +0000

    Allow CORS

--- server/src/app.js
+++ server/src/app.js
@@ -6,6 +6,12 @@ const app = express();
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }))
 
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
 app.set('port', process.env.PORT || 3000);
 app.use(routes);
 
